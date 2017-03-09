766ae267376f60833384c831e752fa44721e34c6
Ante Wall <antewall@gmail.com>
2017-03-09 15:47:11 +0100

    Fixed some tooltip issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -42,6 +42,7 @@ import { ChallengeSettingsComponent } from './settings/challenge-settings/challe
     AppRoutingModule
   ],
   providers: [GameService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
